80d08b555b3be537906e20d8db5a713cf138fad8
Gaetan Lepage <gaetan@glepage.com>
2023-02-12 12:40:05 +0100

    flake

--- flake.nix
+++ flake.nix
@@ -2,44 +2,13 @@
     description = "Server configuration";
 
 
-    inputs = {
-        nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-        simple-nixos-mailserver.url = gitlab:simple-nixos-mailserver/nixos-mailserver;
-        agenix.url = github:ryantm/agenix;
-    };
+    inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
 
-
-    outputs = {
-        self,
-        nixpkgs,
-        simple-nixos-mailserver,
-        agenix
-    }:
-
-    let
-        system = "x86_64-linux";
-
-    in {
-
-        # System configuration
-        nixosConfigurations = {
-
-            server = nixpkgs.lib.nixosSystem {
-                inherit system;
-                modules = [
-                    ./nixos/server
-                    simple-nixos-mailserver.nixosModule {
-                        imports = [ ./nixos/server/mailserver.nix ];
-                    }
-                    agenix.nixosModules.default
-                ];
-            };
-
-            backup = nixpkgs.lib.nixosSystem {
-                inherit system;
-                modules = [ ./nixos/backup ];
-            };
+    outputs = { self, nixpkgs }:
+    {
+        nixosConfigurations.server = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [ ./nixos ];
         };
-
     };
 }
