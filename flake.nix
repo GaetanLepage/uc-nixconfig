babecf1d35f87ffba6df82f44402251d1439b4c4
Gaetan Lepage <gaetan@glepage.com>
2023-05-13 18:40:54 +0200

    flake script as default app

--- flake.nix
+++ flake.nix
@@ -6,10 +6,35 @@
   outputs = {
     self,
     nixpkgs,
-  }: {
-    nixosConfigurations.ultime-pc = nixpkgs.lib.nixosSystem {
+  }: let
+    hostname = "ultime-pc";
+  in {
+    nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [./nixos];
+    };
+
+    apps = let
+      system = "x86_64-linux";
+    in {
+      ${system}.default = {
+        type = "app";
+        program = let
+          pkgs = import nixpkgs {inherit system;};
+        in
+          toString (pkgs.writeScript "generate" ''
+            echo "=> Updating flake inputs"
+            nix flake update
+
+            echo "=> Updating system"
+            nixos-rebuild switch \
+                --verbose \
+                --fast \
+                --flake .#${hostname} \
+                --target-host root@gaming \
+                --build-host root@gaming
+          '');
+      };
     };
 
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
