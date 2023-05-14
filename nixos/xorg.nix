cd6b5aa83e8d228087d78462ddc0899723b9f327
Gaetan Lepage <gaetan@glepage.com>
2023-05-14 22:25:27 +0200

    xorg -> wayland

--- nixos/xorg.nix
+++ nixos/xorg.nix
@@ -1,27 +1,19 @@
 {pkgs, ...}: {
   services.xserver = {
-    # Enable the X11 windowing system.
     enable = true;
 
-    serverFlagsSection = ''
-      Option "BlankTime" "0"
-      Option "StandbyTime" "0"
-      Option "SuspendTime" "0"
-      Option "OffTime" "0"
-    '';
-
-    # Configure keymap in X11
+    # Configure keymap
     layout = "fr";
-
-    # Whether to symlink the X server configuration under /etc/X11/xorg.conf
-    exportConfiguration = true;
 
     # Gnome
     displayManager = {
       gdm = {
         enable = true;
         autoSuspend = false;
+        wayland = true;
       };
+      # Force wayland session by default
+      defaultSession = "gnome";
       autoLogin = {
         enable = true;
         user = "uc";
