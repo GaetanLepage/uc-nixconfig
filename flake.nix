f503715c1f0bd17c6393179993e37c4f2cbbf87d
Gaetan Lepage <gaetan@glepage.com>
2024-03-26 13:03:09 +0100

    flake: add direnv and devshell

--- flake.nix
+++ flake.nix
@@ -1,17 +1,33 @@
 {
-  description = "Server configuration";
+  description = "TV computer configuration";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs = {
     self,
     nixpkgs,
     flake-parts,
+    devshell,
   } @ inputs: let
     hostname = "ultime-tv";
   in
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = nixpkgs.lib.systems.flakeExposed;
+
+      imports = [
+        inputs.devshell.flakeModule
+      ];
 
       flake = {
         nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
@@ -28,19 +44,33 @@
         system,
         ...
       }: {
-        packages.default = with pkgs;
-          writeShellScriptBin "update" ''
-            echo "=> Updating flake inputs"
-            nix flake update
+        devshells.default = {
+          packages = [
+            pkgs.wol
+          ];
 
-            echo "=> Updating system"
-            nixos-rebuild switch \
-                --verbose \
-                --fast \
-                --flake .#${hostname} \
-                --target-host root@tv \
-                --build-host root@tv
-          '';
+          commands = [
+            {
+              name = "wake";
+              command = "wol 90:1b:0e:57:34:a9 -h glepage.com -p 9";
+            }
+            {
+              name = "deploy";
+              command = ''
+                echo "=> Updating flake inputs"
+                nix flake update
+
+                echo "=> Updating system"
+                nixos-rebuild switch \
+                    --verbose \
+                    --fast \
+                    --flake .#${hostname} \
+                    --target-host root@tv \
+                    --build-host root@tv
+              '';
+            }
+          ];
+        };
 
         formatter = pkgs.alejandra;
       };
